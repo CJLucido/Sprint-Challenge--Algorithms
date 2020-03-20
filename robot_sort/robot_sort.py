894c42bd87a06e0c0cf3bc4edd180f4126c08ae1
CJ Lucido <c.jose.lucido@gmail.com>
2020-03-20 11:12:01 -0700

    psuedocode did not translate well, spent too much time planning for one hour maybe. realized I probably should have been trying to implement selection sort too late

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,8 +96,36 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        self.swap_item()
+        #establish that we will be checking every position
+        for i in range(0, len(self._list) - 1): 
+                #if it has reached the end and the index equals the list minus 1
+            if self.can_move_right() == False and i == len(self._list) - 1:
+                    #check the list once more using recursion???
+                self.sort()
+                    #return the completed result
+                return self._list
+               #if it hasn't reached the end
+            elif self.can_move_right():
+                    #move to the right
+                self.move_right()
+                    #compare the held item to the item at that position
+                if self.compare_item() == 1:
+                        #the held item has a greater value so swap it with the item at this position
+                    self.swap_item()
+                        #if the robot can move left then it should move left until it can't anymore (while would be for selection sort- always returning to the beginning, we are just swapping neighbors)
+                    if self.can_move_left(): #changed from while
+                        self.move_left()
+                        #once it has reached the 0th index it should swap the held smaller item to place it at the beginning
+                    self.swap_item()
+                    continue
+                elif self.compare_item() == 0 or self.compare_item()== -1:
+                    if self.can_move_right():
+                        self.move_right()
+                            #compare the items in the same way...should this be a helper function then instead?
+                            #does this break this rule :You may NOT access any instance variables directly. (`self._anything`)
+                        self.sort() #self._list[self._position:]
+        return self._list
 
 
 if __name__ == "__main__":
@@ -110,3 +138,69 @@ if __name__ == "__main__":
 
     robot.sort()
     print(robot._list)
+
+
+    #hierarchy of functions
+        # if compare_item() == None: #none (also can show none if not holding any item)*****
+        # swap_item  needs to be initialized because the robot isn't holding any item at first, grab the first item
+
+
+        # if compare_item() == 1: #greater than
+        # if compare_item() == -1: #less than current held item
+        # if compare_item() == 0: #equal to
+        # if compare_item() == None: #none (also can show none if not holding any item)*****
+        #     swap_item() can always be done, so it needs a condition to tell it when to (that would be compare_item)
+
+        # if can_move_left():
+        #     move_left() is posssible
+
+        # if can_move_right(self):
+        #     move_right() is possible
+
+    
+        # we can control the state of the robot with the light
+        # if it is on we should end the sorting and return the value
+        # else we should continue sorting
+
+
+
+    # what needs to happen, bubble sort approach
+        # have the robot hold an item
+        # compare that item to the one next to it
+        # if the item is greater, swap the items, return to the start and swap again
+        # do this until it reaches the right most edge, then do it again but start from the next index higher,
+        #      BUT if the current index equals the length of the list minus 1 then just return the list
+
+
+    #psuedocode
+        #initialize to first indexed item
+    #         swap_item(self)
+    #     #establish that we will be checking every position
+    #         for i in range(0, len(self._list) - 1): 
+    #             #if it has reached the end and the index equals the list minus 1
+    #             if can_move_right(self) == False and i == len(self._list) - 1:
+    #                 #check the list once more using recursion???
+    #                 sort(self)
+    #                 #return the completed result
+    #                 return self._list
+    #            #if it hasn't reached the end
+    #             elif can_move_right(self):
+    #                 #move to the right
+    #                 move_right(self)
+    #                 #compare the held item to the item at that position
+    #                 if compare_item(self) == 1:
+    #                     #the held item has a greater value so swap it with the item at this position
+    #                     swap_item(self)
+    #                     #if the robot can move left then it should move left until it can't anymore (while would be for selection sort- always returning to the beginning, we are just swapping neighbors)
+    #                     if can_move_left(self): #changed from while
+    #                         move_left(self)
+    #                     #once it has reached the 0th index it should swap the held smaller item to place it at the beginning
+    #                     swap_item(self)
+    #                     continue
+    #                 elif compare(self) == 0 or compare_item(self)== -1:
+    #                     if can_move_right(self):
+    #                         move_right(self)
+    #                         #compare the items in the same way...should this be a helper function then instead?
+    #                         #does this break this rule :You may NOT access any instance variables directly. (`self._anything`)
+    #                         sort(self._list[self._position:])
+    # #damn, I think actually selection sort would be better for this but I can't capture i+1 as a different variable
